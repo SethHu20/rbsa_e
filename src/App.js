c5b54e9db7d77ec30dccfec4cfa2bd716de633ac
Aceboy <andrewli.th611@gmail.com>
2022-04-09 22:04:23 +1000

    added react stuff idk lol

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReportSus from './ReportSus';
+
+function App() {
+  return (
+    <>
+      <ReportSus/>
+      <input type="text"/>
+      <button>Report sus</button>
+    </>
+  )
+}
+
+export default App;
---
+++ src/ReportSus.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ReportSus() {
+  return (
+    <div>Report sus activity</div>
+  )
+}
