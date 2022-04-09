733878ba05eeb21a3c6d4bc012d03e9923a6e593
Aceboy <andrewli.th611@gmail.com>
2022-04-09 23:53:13 +1000

    "Report" function

--- src/ReportSus.js
+++ src/ReportSus.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
+
 export default function ReportSus() {
   return (
-    <div>Report sus activity</div>
+    <> 
+      Title your report:
+      <input type="text"/>
+      <br/><br/>
+      Report Category:<br/>
+      <input type="checkbox"/>Spam<br/>
+      <span><input type="checkbox"/>Advetisement</span><br/>
+      <span><input type="checkbox"/>Potential Scam</span><br/>
+      <br/>
+      <button onClick={()=>{ alert('Report Successful!'); }}>Report</button>
+
+      
+  
+    </>
+
   )
 }
